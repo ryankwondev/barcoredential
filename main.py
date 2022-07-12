c9315f02b5ee98c6930a804dba15aef779536d74
Ryan Kwon <kznm.develop@gmail.com>
2022-07-12 10:54:24 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+import pdf417
+import PIL
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/{string}")
+async def encode(string: str):
+    codes = pdf417.encode(string)
+    image = pdf417.render_image(codes)
+    image.save("image.png")
+    return FileResponse("image.png", media_type="image/png") # Only Image Directory can be used
